4578a0b4ba1ac309d0c7dd3f06197f7eec801e7c
Casey MacLeod <caseymacleod22@gmail.com>
2021-06-22 13:34:53 -0400

    adds stadium model

---
+++ models/stadium.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const stadiumSchema = new Schema({
+    stadium: String,
+    location: String,
+}, {timestamps: true})
+
+module.exports = mongoose.model('Stadium', stadiumSchema)
