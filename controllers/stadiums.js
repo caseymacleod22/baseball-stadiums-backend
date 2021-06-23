daa57dbb6bfe30b260e16605bc5fcc1a7c5a1c87
Casey MacLeod <caseymacleod22@gmail.com>
2021-06-23 12:35:41 -0400

    bug fixes, stadiums post properly

--- controllers/stadiums.js
+++ controllers/stadiums.js
@@ -3,7 +3,7 @@ const Stadium = require('../models/stadium')
 async function index(req, res) {
     try {
         const stadiums = await Stadium.find({})
-        res.status(200).son(stadiums)
+        res.status(200).json(stadiums)
     } catch (error) {
         console.log(error)
         res.status(400).son({error: 'something went wrong'})
@@ -12,7 +12,7 @@ async function index(req, res) {
 
 async function create(req, res) {
     try {
-        const stadium = await Skill.create(req.body)
+        const stadium = await Stadium.create(req.body)
         res.status(201).json(stadium)
     } catch (error) {
         res.status(401).json({error: 'something went wrong'})
