01b8b0325e351b0da0f8851199f9aed4f7eae7ee
Casey MacLeod <caseymacleod22@gmail.com>
2021-07-13 22:58:30 -0400

    :wip: changes to model

--- models/stadium.js
+++ models/stadium.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const stadiumSchema = new Schema({
-    // stadium: String,
+    stadium: String,
     location: String,
 }, {timestamps: true})
 
